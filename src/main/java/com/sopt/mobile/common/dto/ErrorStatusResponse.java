233f3938fa44825f02c73b2afb8b5795ca49a66d
Parkseojin <seojin5565@gmail.com>
2024-05-17 17:10:44 +0900

    setting : success 필드 추가

--- src/main/java/com/sopt/mobile/common/dto/ErrorStatusResponse.java
+++ src/main/java/com/sopt/mobile/common/dto/ErrorStatusResponse.java
@@ -2,12 +2,13 @@ package com.sopt.mobile.common.dto;
 
 public record ErrorStatusResponse(
     int status,
+    boolean success,
     String message
 ) {
   public static ErrorStatusResponse of(int status, String message) {
-    return new ErrorStatusResponse(status, message);
+    return new ErrorStatusResponse(status, false, message);
   }
   public static ErrorStatusResponse of(ErrorMessage errorMessage) {
-    return new ErrorStatusResponse(errorMessage.getStatus(), errorMessage.getMessage());
+    return new ErrorStatusResponse(errorMessage.getStatus(), false, errorMessage.getMessage());
   }
 }
--- src/main/java/com/sopt/mobile/common/dto/SuccessStatusResponse.java
+++ src/main/java/com/sopt/mobile/common/dto/SuccessStatusResponse.java
@@ -3,16 +3,17 @@ package com.sopt.mobile.common.dto;
 
 public record SuccessStatusResponse<T>(
     int status,
+    boolean success,
     String message,
     T result
 ) {
 
   public static SuccessStatusResponse of(SuccessMessage successMessage) {
-    return new SuccessStatusResponse(successMessage.getStatus(), successMessage.getMessage(), null);
+    return new SuccessStatusResponse(successMessage.getStatus(), true, successMessage.getMessage(), null);
   }
 
-  public static <T> SuccessStatusResponse of(SuccessMessage successMessage, T responseDto) {
-    return new SuccessStatusResponse(successMessage.getStatus(), successMessage.getMessage(), responseDto);
+  public static <T> SuccessStatusResponse<T> of(SuccessMessage successMessage, T responseDto) {
+    return new SuccessStatusResponse(successMessage.getStatus(), true, successMessage.getMessage(), responseDto);
   }
 
 }
