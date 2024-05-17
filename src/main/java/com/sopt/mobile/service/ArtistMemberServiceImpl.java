4ad371254f02f33c05b8d0e57a9cccbc69a952b8
Parkseojin <seojin5565@gmail.com>
2024-05-17 18:41:41 +0900

    feat : 목록조회 service 구현 #2

---
+++ src/main/java/com/sopt/mobile/service/ArtistMemberServiceImpl.java
@@ -0,0 +1,39 @@
+package com.sopt.mobile.service;
+
+import com.sopt.mobile.common.dto.ErrorMessage;
+import com.sopt.mobile.domain.ArtistMember;
+import com.sopt.mobile.domain.Member;
+import com.sopt.mobile.exception.NotFoundException;
+import com.sopt.mobile.repository.ArtistMemberRepository;
+import com.sopt.mobile.repository.MemberRepository;
+import com.sopt.mobile.service.dto.response.ArtistMemberFindDto;
+import com.sopt.mobile.service.dto.response.ArtistMemberListFindDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArtistMemberServiceImpl implements ArtistMemberService{
+
+  private final MemberRepository memberRepository;
+  private final ArtistMemberRepository artistMemberRepository;
+
+  @Override
+  public ArtistMemberListFindDto findSubscribedArtists(Long memberId) {
+
+    // 멤버 찾기
+    Member findMember = memberRepository.findById(memberId).orElseThrow(
+        () -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND_BY_ID_EXCEPTION)
+    );
+
+    // 아티스트 찾기
+    List<ArtistMemberFindDto> isSubsArtists =  artistMemberRepository.findSubscribedArtists(findMember)
+        .stream().map(ArtistMemberFindDto::of).toList();
+
+    List<ArtistMemberFindDto> isNotSubscribedArtists =  artistMemberRepository.findNotSubscribedArtists(findMember)
+        .stream().map(ArtistMemberFindDto::of).toList();
+
+    return ArtistMemberListFindDto.of(isSubsArtists, isNotSubscribedArtists);
+  }
+}
