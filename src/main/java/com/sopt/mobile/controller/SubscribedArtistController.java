8532be5b10b72cd120b825638788a197b2e40213
Parkseojin <seojin5565@gmail.com>
2024-05-17 20:36:31 +0900

    feat : 즐겨찾기 삭제 컨트롤러 #3

---
+++ src/main/java/com/sopt/mobile/controller/SubscribedArtistController.java
@@ -0,0 +1,33 @@
+package com.sopt.mobile.controller;
+
+
+import com.sopt.mobile.common.dto.SuccessMessage;
+import com.sopt.mobile.common.dto.SuccessStatusResponse;
+import com.sopt.mobile.service.SubscribedArtistService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/vi/artists/artist-members/friend")
+@RequiredArgsConstructor
+public class SubscribedArtistController {
+
+  private final SubscribedArtistService subscribedArtistService;
+
+  @DeleteMapping("/{artistMemberId}")
+  public ResponseEntity<SuccessStatusResponse> deleteSubscribedArtist(
+      @RequestHeader(name = "memberId") Long memberId,
+      @PathVariable(name = "artistMemberId") Long artistMemberId) {
+
+
+    subscribedArtistService.removeSubscribedArtist(memberId, artistMemberId);
+    return ResponseEntity.ok(SuccessStatusResponse.of(
+        SuccessMessage.ARTIST_MEMBER_FIND_SUCCESS ));
+  }
+}
