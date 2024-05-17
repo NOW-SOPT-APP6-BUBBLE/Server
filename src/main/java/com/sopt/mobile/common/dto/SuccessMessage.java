8db8ca0cea68ee0942c91dec8f295458b81c34a1
Parkseojin <seojin5565@gmail.com>
2024-05-17 20:52:09 +0900

    feat : subscribedArtist 관련 response 메시지 추가  #3

--- src/main/java/com/sopt/mobile/common/dto/SuccessMessage.java
+++ src/main/java/com/sopt/mobile/common/dto/SuccessMessage.java
@@ -1,5 +1,6 @@
 package com.sopt.mobile.common.dto;
 
+import com.sopt.mobile.service.SubscribedArtistService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -10,7 +11,8 @@ public enum SuccessMessage {
 
   BLOG_CREATE_SUCCESS(HttpStatus.CREATED.value(), "블로그 생성이 완료되었습니다."),
   ARTIST_MEMBER_FIND_SUCCESS(HttpStatus.OK.value(), "아티스트 멤버 목록 조회 성공했습니다."),
-  ARTISTS_FIND_SUCCESS(HttpStatus.FOUND.value(), "아티스트 목록 조회에 성공했습니다.");
+  ARTISTS_FIND_SUCCESS(HttpStatus.FOUND.value(), "아티스트 목록 조회에 성공했습니다."),
+  SUBSCRIBED_ARTISTS_DELETE_SUCCESS(HttpStatus.OK.value(), "구독 중인 아티스트 제거 성공했습니다.");
 
   private final int status;
   private final String message;
