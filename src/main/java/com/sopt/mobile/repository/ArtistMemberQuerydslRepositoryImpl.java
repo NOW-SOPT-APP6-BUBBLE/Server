46170f6f404a339ef02c60107089f8f385187be8
Parkseojin <seojin5565@gmail.com>
2024-05-17 22:39:20 +0900

    feat : profile 조회 관련 querydsl(구현 실패- 더 생각해보고 나중에 리팩토링할 예정) #4

--- src/main/java/com/sopt/mobile/repository/ArtistMemberQuerydslRepositoryImpl.java
+++ src/main/java/com/sopt/mobile/repository/ArtistMemberQuerydslRepositoryImpl.java
@@ -3,11 +3,14 @@ package com.sopt.mobile.repository;
 import static com.sopt.mobile.domain.QArtistMember.artistMember;
 import static com.sopt.mobile.domain.QSubscribedArtist.subscribedArtist;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sopt.mobile.domain.ArtistMember;
 import com.sopt.mobile.domain.Member;
+import com.sopt.mobile.service.dto.response.ArtistMemberDetailFindDto;
 import jakarta.persistence.EntityManager;
 import java.util.List;
+import java.util.Optional;
 
 public class ArtistMemberQuerydslRepositoryImpl implements ArtistMemberQuerydslRepository {
 
@@ -43,4 +46,30 @@ public class ArtistMemberQuerydslRepositoryImpl implements ArtistMemberQuerydslR
         )
         .fetch();
   }
+
+//  Long artistMemberId,
+//  String nickname,
+//  String imageURL,
+//  String introduction,
+//  boolean isSubscribed,
+//  String artistName,
+//  String artistMemberName
+//  public Optional<ArtistMemberDetailFindDto> findArtistMemberDetail(Long artistMemberId){
+//    return queryFactory
+//        .select(Projections.constructor(ArtistMemberDetailFindDto.class,
+//            artistMember.id,
+//            artistMember.nickname,
+//            artistMember.imageURL,
+//            artistMember.introduction,
+//            subscribedArtist.id.isNotNull().as("isSubscribed") // SubscribedArtist 존재 여부에 따라 true/false
+//            subscribedArtist.name,
+//            su
+//        ))
+//        .from(artistMember)
+//        .leftJoin(subscribedArtist).on(subscribedArtist.artistMember.id.eq(artistMember.id)
+//            .and(subscribedArtist.artistMember.id.eq(artistMemberId)))
+//        .where(artistMember.id.eq(artistMemberId), artistMember.isService.isTrue())
+//        .fetch();
+//
+//  }
 }
