f21657ebc67a9f89cd37f23f67909a46284947d3
Parkseojin <seojin5565@gmail.com>
2024-05-17 22:38:24 +0900

    feat : profile 조회 컨트롤러 구현  #4

--- src/main/java/com/sopt/mobile/controller/ArtistMemberController.java
+++ src/main/java/com/sopt/mobile/controller/ArtistMemberController.java
@@ -7,6 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +28,16 @@ public class ArtistMemberController {
     return ResponseEntity.ok(SuccessStatusResponse.of(
         SuccessMessage.ARTIST_MEMBER_FIND_SUCCESS,
         artistMemberService.findSubscribedArtists(memberId)));
+  }
+
+  @GetMapping("{artistMemberId}")
+  public ResponseEntity<SuccessStatusResponse> getArtistMemberDetail(
+      @RequestHeader(name = "memberId") Long memberId,
+      @PathVariable(name = "artistMemberId") Long artistMemberId){
+
+    return ResponseEntity.ok(SuccessStatusResponse.of(
+        SuccessMessage.ARTIST_MEMBER_DETAIL_FIND_SUCCESS,
+        artistMemberService.findArtistMemberDetail(memberId, artistMemberId)));
   }
 
 }
