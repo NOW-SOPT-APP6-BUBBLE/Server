b471ad7b504f974529b2e2aea95c41f580e71b75
Parkseojin <seojin5565@gmail.com>
2024-05-17 18:41:53 +0900

    feat : 목록조회 controller, 리턴 메시지 구현 #2

--- src/main/java/com/sopt/mobile/controller/ArtistMemberController.java
+++ src/main/java/com/sopt/mobile/controller/ArtistMemberController.java
@@ -2,12 +2,11 @@ package com.sopt.mobile.controller;
 
 import com.sopt.mobile.common.dto.SuccessMessage;
 import com.sopt.mobile.common.dto.SuccessStatusResponse;
+import com.sopt.mobile.service.ArtistMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,23 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ArtistMemberController {
 
-  private final ArtistMemberService ArtistMemberService;
+  private final ArtistMemberService artistMemberService;
 
   @GetMapping("")
-  public ApiResponse<MemberResponseDto.nicknameDto> getSubscribedArtists(
+  public ResponseEntity<SuccessStatusResponse> getSubscribedArtists(
       @RequestHeader(name = "memberId") Long memberId){
 
-    return SuccessStatusResponse.of(
-        HttpStatus.OK,
-        SuccessMessage.
-        ArtistMemberService.findSubscribedArtists(memberId));
-
-    if(!memberRequestDto.getNickname().isEmpty()) {
-      MemberResponseDto.nicknameDto result = memberService.patchNickname(member, memberRequestDto);
-      return ApiResponse.onSuccess(result);
-    } else
-      throw new ExceptionHandler(NICKNAME_EMPTY);
+    return ResponseEntity.ok(SuccessStatusResponse.of(
+        SuccessMessage.ARTIST_MEMBER_FIND_SUCCESS,
+        artistMemberService.findSubscribedArtists(memberId)));
   }
-
 
 }
