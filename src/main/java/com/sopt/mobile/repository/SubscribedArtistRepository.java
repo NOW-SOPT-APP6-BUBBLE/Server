df20149ec05d6059f8f703b6b425d886e0c10d5a
Parkseojin <seojin5565@gmail.com>
2024-05-17 20:36:16 +0900

    refactor : 즐겨찾기 삭제 리포지토리에 쿼리 생성 #3

--- src/main/java/com/sopt/mobile/repository/SubscribedArtistRepository.java
+++ src/main/java/com/sopt/mobile/repository/SubscribedArtistRepository.java
@@ -1,9 +1,19 @@
 package com.sopt.mobile.repository;
 
 import com.sopt.mobile.domain.ArtistMember;
+import com.sopt.mobile.domain.Member;
 import com.sopt.mobile.domain.SubscribedArtist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SubscribedArtistRepository extends JpaRepository<SubscribedArtist, Long> {
+
+  @Modifying
+  @Query("delete from SubscribedArtist s where s.artistMember = :artistMember AND s.member = :member")
+  void deleteSubscribedArtist(@Param("member") Member member, @Param("artistMember") ArtistMember artistMember);
+
+  boolean existsByMemberAndArtistMember(Member member, ArtistMember artistMember);
 
 }
